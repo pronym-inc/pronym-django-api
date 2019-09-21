3bffb10c76f2b6a39314df22978f2520b21050ac
Gregg Keithley <gregg@pronym.com>
2019-09-21 13:54:38 -0400

    Setup py updates.

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     url='https://github.com/greggg230/pronym-django-api',
     author='Pronym',
     author_email='gregg@pronym.com',
-    packages=find_packages('pronym_api'),
+    packages=find_packages(where='pronym_api'),
+    package_dir={
+        '': 'pronym_api'
+    },
     install_requires=['django==2.2.4', 'django-nose==1.4.6'],
     version='0.1',
     license='MIT',
