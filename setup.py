1f5712ebb1e095c49310b7668ca7cc27f4987ce1
Gregg Keithley <gregg@pronym.com>
2019-11-10 13:56:15 -0500

    Updated release.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=install_dependencies,
     tests_require=test_dependencies,
     extras_require={'test': test_dependencies},
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description=(
         'A thin wrapper around Django views for easily customized '
