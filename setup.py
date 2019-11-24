7358fd5c8d4e4f71ff9ad1a82b11d094db95a6e8
Gregg Keithley <gregg@pronym.com>
2019-11-24 13:41:26 -0500

    Missing file

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=install_dependencies,
     tests_require=test_dependencies,
     extras_require={'test': test_dependencies},
-    version='1.0.7',
+    version='1.0.8',
     license='MIT',
     description=(
         'A thin wrapper around Django views for easily customized '
