8f145a13ddbba51decfe7579bd45b161257a864b
Gregg Keithley <gregg@pronym.com>
2020-11-08 12:04:46 -0500

    Add name as str function for permission

--- pronym_api/models/api_permission.py
+++ pronym_api/models/api_permission.py
@@ -4,3 +4,6 @@ from django.db import models
 class ApiPermission(models.Model):
     """A permission for the API."""
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
